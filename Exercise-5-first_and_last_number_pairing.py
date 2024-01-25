7307731d59b82e5080d1d9ce76d3489d20ebba5e
unknown <miggy.payumo26@gmail.com>
2024-01-26 04:22:09 +0800

    Pseudocode

---
+++ Exercise-5-first_and_last_number_pairing.py
@@ -0,0 +1,5 @@
+# Pseudocode
+# 1. Ask the user to input a set of numbers
+# 2. Evaluate the numbers given by the user
+# 3. Check to see if the first and last number of the set is the same 
+# 4. If yes return "True" if no return "False"
