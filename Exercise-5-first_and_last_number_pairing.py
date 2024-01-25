3ab5de715f2322c604e3b509246f5e29e8a9b3e5
unknown <miggy.payumo26@gmail.com>
2024-01-26 04:44:40 +0800

    Code will now return true or false depending on the number set

--- Exercise-5-first_and_last_number_pairing.py
+++ Exercise-5-first_and_last_number_pairing.py
@@ -4,5 +4,14 @@
 # 3. Check to see if the first and last number of the set is the same 
 # 4. If yes return "True" if no return "False"
 
-def firstval_lastval_isequal(numberList):
-    print("Given list: ", numberList)
+def firstval_lastval_isequal(number_set):
+    print("Given list: ", number_set)
+
+    first_val = number_set[0]
+    last_val = number_set[-1]
+
+    if first_val == last_val:
+        return True
+    else:
+        return False
+    
