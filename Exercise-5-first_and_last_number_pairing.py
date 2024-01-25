04a629d02ad5b115f5828742be7727271b7f6b21
unknown <miggy.payumo26@gmail.com>
2024-01-26 04:26:46 +0800

    Code can now accept integer input

--- Exercise-5-first_and_last_number_pairing.py
+++ Exercise-5-first_and_last_number_pairing.py
@@ -3,3 +3,12 @@
 # 2. Evaluate the numbers given by the user
 # 3. Check to see if the first and last number of the set is the same 
 # 4. If yes return "True" if no return "False"
+
+while True:
+    set_input = input("Please input a set of numbers: ")
+    if set_input.isdigit():
+        set_input = int(set_input)
+    else:
+        print("Invalid, please input numbers.")
+        
+        break
